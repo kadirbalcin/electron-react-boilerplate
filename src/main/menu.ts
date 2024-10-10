62bb3e1ab0e7e64fd818e812a094f5df31185516
Kadir Balçın <kadirbalcin145@gmail.com>
2024-10-10 16:09:29 +0300

    initial commit

--- src/main/menu.ts
+++ src/main/menu.ts
@@ -54,10 +54,10 @@ export default class MenuBuilder {
 
   buildDarwinTemplate(): MenuItemConstructorOptions[] {
     const subMenuAbout: DarwinMenuItemConstructorOptions = {
-      label: 'Electron',
+      label: 'Turvigo',
       submenu: [
         {
-          label: 'About ElectronReact',
+          label: 'Uygulama Hakkında',
           selector: 'orderFrontStandardAboutPanel:',
         },
         { type: 'separator' },
--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,21 +1,20 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
-import './App.css';
+
+import './'
 
 function Hello() {
   return (
     <div>
       <div className="Hello">
-        <img width="200" alt="icon" src={icon} />
       </div>
       <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
+      <div className="Hello flex flex-col">
         <a
           href="https://electron-react-boilerplate.js.org/"
           target="_blank"
           rel="noreferrer"
         >
-          <button type="button">
+          <button type="button" className='bg-black'>
             <span role="img" aria-label="books">
               📚
             </span>
